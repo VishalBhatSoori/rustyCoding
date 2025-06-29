29f1ab8211fee62101d58e6d8ce4af458127c3e1
Vishal Bhat Soori <vishalbhatsoori@gmail.com>
2025-06-29 11:44:49 +0000

    ininital commit for rustyOwnership

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    
+    let a = 18;
+    let b = a;
+    println!("a = {}",a);
+    println!("b = {}",b);
+
+    let my_name:String = String ::from("Hello from Heap Memory");
+    let your_name = my_name;
+    println!("my_name = {}",my_name);
+    println!("your_name= {}",your_name);
+
+
+}
